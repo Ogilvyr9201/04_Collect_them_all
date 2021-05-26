2db4d38f9e9a1b986424078d7a7a7a549c97c5d4
Ogilvyr9201 <78511761+Ogilvyr9201@users.noreply.github.com>
2021-05-26 13:24:21 +1200

    Item cost and trial checker complete

---
+++ 02_cta_item_cost.py
@@ -0,0 +1,43 @@
+# Functions go here
+def num_check(question, error, low=None, high=None):
+
+    valid = False
+    while not valid:
+        try:
+            response = float(input(question))
+
+            if low is not None and high is not None:
+                if low < response < high:
+                    return response
+                else:
+                    print(error)
+                    print()
+                    continue
+
+            elif low is not None:
+                if response > low:
+                    return response
+                else:
+                    print(error)
+                    print()
+                    continue
+
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+            print()
+
+
+# Main Routine
+# Looped fpr testing
+while 1 == 1:
+    # Ask user for the item
+    item_error = "<error> enter the price eg: 4 or 4.5"
+    item_cost = num_check("What is the cost of the Item? ", item_error, 0)
+    # Output item back (credit to https://www.python-course.eu/python3_formatted_output.php 
+    # for info on how to out put variable as a float)
+    print("Item Cost: $%2.2f"%(item_cost))
+    print()
+    break
---
+++ 03_trial_checker_v2.py
@@ -0,0 +1,45 @@
+# Functions go here
+# Functions go here
+def num_check(question, error, num_type, low=None, high=None):
+
+    valid = False
+    while not valid:
+        try:
+            response = num_type(input(question))
+
+            if low is not None and high is not None:
+                if low < response < high:
+                    return response
+                else:
+                    print(error)
+                    print()
+                    continue
+
+            elif low is not None:
+                if response > low:
+                    return response
+                else:
+                    print(error)
+                    print()
+                    continue
+
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+            print()
+
+# Main Routine
+# looped for testing
+while 1 == 1:
+    # Asks user for amount of trials
+    trial_error = "<error> Please enter an interger above 0"
+    num_trials = num_check("What is the numebr of trials? ", trial_error, int, 0)
+    price = num_check("What is the cost? ", "Please enter an amount more than zero", float, 0)
+    # Sets number too an interger without decimal 
+    # num_trials = int(num_trials)
+    # Outputs the number of trials (For testing)
+    print("Number of trials", num_trials)
+    print()
+    break
